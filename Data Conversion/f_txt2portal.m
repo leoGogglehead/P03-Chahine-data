ad5c8277ade7c4edd4156b0e9af155d61337072b
jtmoyer <jtm197@gmail.com>
2015-03-30 13:04:01 -0400

    Working code to upload annotations from .txt file

--- Data Conversion/f_txt2portal.m
+++ Data Conversion/f_txt2portal.m
@@ -18,7 +18,7 @@ function [] = f_txt2portal(dataset, animalDir, layerName)
   %   USAGE:
   %       f_eeg2mef('Z:\public\DATA\Animal_Data\DichterMAD\r097\Hz2000',0.1,10000,10);
   %
-   dbstop in f_txt2portal at 55
+%    dbstop in f_txt2portal at 70
 
   %....... Load .txt annotations  
   txtDir = fullfile(animalDir,'');
@@ -47,94 +47,87 @@ function [] = f_txt2portal(dataset, animalDir, layerName)
   % extract label, time, and duration values for each annotation
   % remove any labels that don't have a duration value - they are redundant
   keepInds = ~cellfun(@isempty,strtrim(textdata{3}));
-  annotation = strtrim(textdata{1}(keepInds));
+  
+  % get annotation text
+  annotations = strtrim(textdata{1}(keepInds));
+  
+  % get annotations times and convert to microseconds (file starts at 0:00)
   times = textdata{2}(keepInds);
   times = datenum(times, 'HH:MM:SS AM') - dateOffset;
   % convert to Usec; times after midnight need to be set to the next day
-  timesUsec = (times - sign(times) + 1) * 24 * 60 * 60 * 1e6;
-  duration = cellfun(@strsplit,textdata{3}(:),'UniformOutput',false);
-  
-  allLabels = cell(1,3);
-  for f = 1:size(revList,1) 
-    % open and scan .rev file
-    fid = fopen(revList{f,1});   
-    revText = textscan(fid,'%f %*[^:]:%[^;];%*[^;];%*[^:]:%[^<]<%*[^>]>');
-    fclose(fid);
-    
-    % open and scan associated .bni file
-    try % try using the .bni extension
-      bni_name = [revList{f,2} '.bni'];
-      fid=fopen(bni_name);   % METADATA IN BNI FILE
-      metadata=textscan(fid,'%s = %s %*[^\n]');
-      fclose(fid);
-    catch % if problem, try using the .bni_orig extension
-      bni_name = [revList(f) '.bni_orig'];
-      fid=fopen(bni_name);   % METADATA IN BNI FILE
-      assert(fid > 0, 'Check BNI file exists: %s\n', rev250List(f).name);
-      metadata=textscan(fid,'%s = %s %*[^\n]');
-      fclose(fid);
-    end
-    recordDate = char(metadata{1,2}(strcmp(metadata{:,1}, 'Date')));
-    recordTime = char(metadata{1,2}(strcmp(metadata{:,1}, 'Time')));
-    dateNumber = datenum(sprintf('%s %s', recordDate, recordTime), dateFormat);
-    startTime = (dateNumber - dateOffset) * 24 * 3600 * 1e6; % in microseconds
-    
-    revText{1} = revText{1}*1e6 + startTime;
-    revText{2} = strtrim(revText{2});
-    revText{3} = strtrim(revText{3});
-    % datestr(datenum(revText{1}/1e6/3600/24)+dateOffset-1)
-    for i = 1: 3
-      allLabels{i} = [allLabels{i}; revText{i}];
-    end
+  times(times<0) = times(times<0) + 1;
+  timesUsec = (times) * 24 * 60 * 60 * 1e6; %   check = datestr(timesUsec/1e6/60/60/24);
+
+  % get duration of each annotation from text file
+  % duration is the first cell, ignore other values & convert to usec
+  numStrings = cellfun(@strsplit,textdata{3}(keepInds),'UniformOutput',false);
+  duration = zeros(length(numStrings),1);
+  for d = 1:length(numStrings)
+    duration(d) = str2num(char(numStrings{d}(1))) * 1e6;
   end
   
+  % assign annotations to different layers 
+  layers = cell(length(annotations),1);
+  for d = 1: length(annotations)
+    if strcmp(annotations{d}, 'Wake')
+      layers{d} = 'Wake';
+    elseif strcmp(annotations{d}, 'NREM 1')
+      layers{d} = 'NREM1';
+    elseif strcmp(annotations{d}, 'NREM 2')
+      layers{d} = 'NREM2';
+    elseif strcmp(annotations{d}, 'NREM 3')
+      layers{d} = 'NREM3';
+    elseif strcmp(annotations{d}, 'REM')
+      layers{d} = 'REM';
+    elseif (strcmp(annotations{d},'Lights On') || strcmp(annotations{d},'Lights Off'))
+      layers{d} = 'Lights';
+    elseif (strcmp(annotations{d},'CPAP 0') || strcmp(annotations{d},'Diagnostic'))
+      layers{d} = 'CPAP';
+    else
+      layers{d} = 'Notes';
+    end
+  end
+    
   %....... Add annotations to portal
-  eventTimes = allLabels{1};
-  eventChannels = allLabels{2}; % 'L_DG', 'ch_01', 'ch_02',
-  eventLabels = allLabels{3};
-  
-  % save unique channels and labels to a text file for reference
-  uniqueLabels = unique(eventLabels);
-  uniqueChannels = unique(eventChannels);
-  fid = fopen(fullfile(txtDir, [animalDir(39:42) '.txt']),'w');
-  fprintf(fid, 'uniqueChannels\r\n');
-  fprintf(fid, '%s\r\n', uniqueChannels{:});
-  fprintf(fid, '\r\nuniqueLabels\r\n');
-  fprintf(fid, '%s\r\n', uniqueLabels{:});
-  fclose(fid);
-  
-  fprintf('\nAnimal: %s\n', animalDir(39:42));
+  eventTimes = timesUsec;
+  eventLabels = annotations;
+    
+  fprintf('\Subject: %s\n', animalDir(40:end));
+
   % upload annotations to dataset
   % remove old layer and add new one
-  try 
-    fprintf('Removing existing layer\n');
-    dataset.removeAnnLayer(layerName);
-  catch 
-    fprintf('No existing layer\n');
+  uniqueLayers = unique(layers);
+  
+  for d = 1: length(uniqueLayers)
+    layerName = uniqueLayers{d};
+    for i = 1: length(layers)
+      addThese(i) = strcmp(layers(i), layerName);
+    end
+    
+    try
+      fprintf('Removing existing layer: %s\n', layerName);
+      dataset.removeAnnLayer(layerName);
+    catch 
+      fprintf('No existing layer\n');
+    end
+    annLayer = dataset.addAnnLayer(layerName);
+
+    % create annotations
+    fprintf('Creating annotations...');
+    ann = IEEGAnnotation.createAnnotations(eventTimes(addThese), ...
+      eventTimes(addThese)+duration(addThese), ...
+      'Event', eventLabels(addThese), dataset.channels(1));
+    fprintf('done!\n');
+
+    %add annotations 5000 at a time (freezes if adding too many)
+    numAnnot = numel(ann);
+    startIdx = 1;
+    fprintf('Adding annotations...\n');
+    for i = 1:ceil(numAnnot/5000)
+      fprintf('Adding %d to %d\n',startIdx,min(startIdx+5000,numAnnot));
+      annLayer.add(ann(startIdx:min(startIdx+5000,numAnnot)));
+      startIdx = startIdx+5000;
+    end
+    fprintf('done!\n');
   end
-  annLayer = dataset.addAnnLayer(layerName);
-
-  % create annotations
-  fprintf('Creating annotations...');
-%  ann = IEEGAnnotation.createAnnotations(eventTimes, eventTimes+5*1e6, 'Event', eventLabels, dataset.channels(1));
-  ann = IEEGAnnotation.createAnnotations(eventTimes, eventTimes+1*1e6, 'Event', eventLabels, dataset.channels(1));
-  fprintf('done!\n');
-
-%   for i = 1:numel(uniqueAnnotChannels)
-%     tmpChan = uniqueAnnotChannels(i);
-%     ann = [ann IEEGAnnotation.createAnnotations(eventTimesUSec(eventChannels==tmpChan,1), ...
-%       eventTimesUSec(eventChannels==tmpChan,2),'Event', ...
-%       params.label,dataset.channels(tmpChan))];
-%   end
-
-  %add annotations 5000 at a time (freezes if adding too many)
-  numAnnot = numel(ann);
-  startIdx = 1;
-  fprintf('Adding annotations...\n');
-  for i = 1:ceil(numAnnot/5000)
-    fprintf('Adding %d to %d\n',startIdx,min(startIdx+5000,numAnnot));
-    annLayer.add(ann(startIdx:min(startIdx+5000,numAnnot)));
-    startIdx = startIdx+5000;
-  end
-  fprintf('done!\n');
 end
